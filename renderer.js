b51b08ffb374d12af84fde8b1b95086c9368a271
Josh Simmons <jsimmons@udel.edu>
2015-04-26 01:28:08 -0400

    changed the game

--- renderer.js
+++ renderer.js
@@ -1,8 +1,16 @@
 var jade = require('jade');
+var fs = require('fs');
 var JSONparams = require('./routes/example1/JSONparams')
 var injects = require('./routes/example1/injects');
-JSONparams.pretty = true;
-injects.pretty = true;
-var fn = jade.renderFile('views/frameDiv.jade',injects);
+var style = '';
+fs.readFile('./public/stylesheets/frame.css','utf8',function(err,data){
+	if(err) {
+		return console.log(err);
+	}
+	style = data;
+	injects.style = style;
+	injects.pretty = true;
+	var fn = jade.renderFile('views/frameDiv.jade',injects);
 
-console.log(fn);
+	console.log(fn);
+});
