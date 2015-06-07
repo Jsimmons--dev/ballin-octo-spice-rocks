89dce1be65e7d1d203b488a3a76cf0bb9f0b95bf
jsimmons--dev <jsimmons@udel.edu>
2015-06-07 16:19:57 -0400

    condensed files

--- renderer.js
+++ renderer.js
@@ -3,14 +3,14 @@ var fs = require('fs');
 var JSONparams = require('./routes/example1/JSONparams')
 var injects = require('./routes/example1/injects');
 var style = '';
-fs.readFile('./public/stylesheets/frame.css','utf8',function(err,data){
+fs.readFile('./public/stylesheets/frame.css',{encoding: 'utf-8'},function(err,data){
 	if(err) {
 		return console.log(err);
 	}
 	style = data;
 	injects.style = style;
 	injects.pretty = true;
-	var fn = jade.renderFile('views/frameDiv.jade',injects);
+	var fn = jade.renderFile('views/frame.jade',injects);
 
 	console.log(fn);
 });
